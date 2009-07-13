8a8a71f56be81212a2c51b9774dab291685943b4
Eric Jonas <jonas@mit.edu>
2009-07-13 12:30:38 -0400

    Created log level string parser.

--- src/logging.cc
+++ src/logging.cc
@@ -1,111 +1,25 @@
 #include "logging.h"
 
-int config_logging(po::variables_map vm, std::string LOGROOT) {
-  /*
-    Process the variables map and configure logging. I feel like we should
-    abstract this away somehow, but I'm not really too concerned right now
-
-  */
-
-  // configure logging
-  std::string propfile; 
-  if (vm.count("log-properties")) {
-    log4cpp::PropertyConfigurator::configure(vm["log-properties"].as<string>()); 
-    
-  } 
-  
-  log4cpp::Priority::Value loglevel = log4cpp::Priority::WARN; 
-
-  int custlevel; 
-  if (vm.count("log-level")) {
-    custlevel = vm["log-level"].as<int>(); 
-    loglevel = intToPriority(custlevel); 
-  } 
-  
-  log4cpp::Appender * logappender =  new log4cpp::OstreamAppender("appender", &std::cout); 
-  log4cpp::Layout* loglayout = new log4cpp::BasicLayout();
-  logappender->setLayout(loglayout); 
-  log4cpp::Category::getRoot().setPriority(loglevel); 
-  log4cpp::Category& root = log4cpp::Category::getRoot();
-  
-  root.addAppender(logappender); 
-
-}
-
-po::options_description logging_desc() 
+boost::logging::level lt = log_type_parse(std::string l)
 {
+  if (l == "debug") {
+    return boost::logging::level::debug;
+  }
 
-  po::options_description loggingdesc("Logging options");
-  loggingdesc.add_options()
-    ("log-properties", po::value<string>(), "A custom log configuration file")
-    ("log-level", po::value<int>()->default_value(3), 
-     "The minimum log level to log to standard out; 0=lowest (log everything), 9 =highest (log nothing)"); 
+  if (l == "info") {
+    return boost::logging::level::info;
+  }
 
-  return loggingdesc; 
+  if (l == "warning") {
+    return boost::logging::level::warning;
+  }
 
-}
+  if (l == "error") {
+    return boost::logging::level::error;
+  }
 
-log4cpp::Priority::Value intToPriority(int custlevel) {
-
-  log4cpp::Priority::Value  loglevel; 
-
-
-    switch(custlevel) {
-    case 0:  
-      return  log4cpp::Priority::DEBUG;  
-      break; 
-    case 1:  
-      return log4cpp::Priority::INFO;  
-      break; 
-    case 2:  
-      return log4cpp::Priority::NOTICE;  
-      break; 
-    case 3:  
-      return log4cpp::Priority::WARN;  
-      break; 
-    case 4:  
-      return log4cpp::Priority::ERROR;  
-      break; 
-    case 5:  
-      return log4cpp::Priority::CRIT;  
-      break; 
-    case 6:  
-      return log4cpp::Priority::ALERT;  
-      break; 
-    default:
-      break; 
-    }
-    
-    return log4cpp::Priority::FATAL;  
-}
-
-
-int  priorityToInt(log4cpp::Priority::Value v) {
-
-  switch(v) {
-  case log4cpp::Priority::DEBUG: 
-    return 0; 
-    break; 
-  case log4cpp::Priority::INFO:
-    return 1; 
-    break; 
-  case log4cpp::Priority::NOTICE:
-    return 2; 
-    break; 
-  case log4cpp::Priority::WARN:
-    return 3; 
-    break; 
-  case log4cpp::Priority::ERROR:
-    return 4; 
-    break; 
-  case log4cpp::Priority::CRIT:
-    return 5; 
-    break; 
-  case log4cpp::Priority::ALERT:
-    return 6; 
-    break; 
-  default: 
-    return -1; 
+  if (l == "fatal") {
+    return boost::logging::level::fatal;
   }
 
 }
--- src/logging.h
+++ src/logging.h
@@ -1,26 +1,10 @@
-#ifndef LOGGING_H
-#define LOGGING_H
+#ifndef __LOGGING_LOG_UTIL_H__
+#define __LOGGING_LOG_UTIL_H__
 
+#include <boost/logging/format_fwd.hpp>
+#include <boost/logging/format/named_write.hpp>
 
+boost::logging::level lt = log_type_parse(std::string l);
 
-#include <log4cpp/Category.hh>
-#include <log4cpp/PropertyConfigurator.hh>
-#include <log4cpp/Appender.hh>
-#include <log4cpp/OstreamAppender.hh>
-#include <log4cpp/Layout.hh>
-#include <log4cpp/BasicLayout.hh>
-
-#include <boost/program_options.hpp>
-namespace po = boost::program_options;
-using namespace std; 
-
-
-int config_logging(po::variables_map vm, std::string LOGROOT); 
-
-po::options_description logging_desc(); 
-
-log4cpp::Priority::Value intToPriority(int custlevel); 
-
-int  priorityToInt(log4cpp::Priority::Value v); 
 
 #endif // LOGGING_H
